bd99cf1c4b8644170c082852d5278a7e8fd75a25
KristinWolv <162977890+KristinWolv@users.noreply.github.com>
2024-08-03 12:29:13 +0200

    Added rest of code

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,15 @@
-// took out React impost since we are on React 18
+import React from "react";
 
 export default function Header() {
     return (
         <header className="header">
-            
+
+            <img 
+            src="./images/troll-face.png"
+            className="header--image"/>
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">DJS07</h4>
-        </header>
+      </header>
+        
     )
-}
+ }
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-// took out the React import since we are on React 18
-import Header from "./components/Header"
-export default function App() {
-    return (
-      <div>
-          <Header />
-      </div>
-  )  
+import React from "react"
+import Header from "./components/Header.jsx"
+import Meme from "./components/Meme.jsx"
+
+function App() {
+  return (
+    <div>
+      <Header />
+     <Meme />
+    </div>
+  )
 }
+
+export default App
 
 
