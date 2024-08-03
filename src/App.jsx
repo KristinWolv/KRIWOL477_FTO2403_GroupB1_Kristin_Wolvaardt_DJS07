deba159fb0fd993ec892a7b6825f2988548e4675
KristinWolv <162977890+KristinWolv@users.noreply.github.com>
2024-08-03 12:01:18 +0200

    scrimba

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+// took out the React import since we are on React 18
+import Header from "./components/Header"
+export default function App() {
+    return (
+      <div>
+          <Header />
+      </div>
+  )  
 }
 
-export default App
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// took out React impost since we are on React 18
+
+export default function Header() {
+    return (
+        <header className="header">
+            
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">DJS07</h4>
+        </header>
+    )
+}
